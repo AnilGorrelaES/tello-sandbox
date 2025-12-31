946d215121bf84021ff31b18227b30f8cc4a6444
Anil esainvdt01 <anil.gorrela@eternal-solutions.com>
2025-12-30 19:56:01 -0500

    fixing code for python 3.10.x

--- tello_face_tracking.py
+++ tello_face_tracking.py
@@ -76,85 +76,95 @@ def track_face_in_video_feed(exit_event, show_video_conn, video_writer_conn, run
     pan_pid.initialize()
     tilt_pid.initialize()
 
-    while not exit_event.is_set():
-        frame = frame_read.frame
+    try:
+        while not exit_event.is_set():
+            frame = frame_read.frame
 
-        frame = imutils.resize(frame, width=400)
-        H, W, _ = frame.shape
+            frame = imutils.resize(frame, width=400)
+            H, W, _ = frame.shape
 
-        # calculate the center of the frame as this is (ideally) where
-        # we will we wish to keep the object
-        centerX = W // 2
-        centerY = H // 2
+            # calculate the center of the frame as this is (ideally) where
+            # we will we wish to keep the object
+            centerX = W // 2
+            centerY = H // 2
 
-        # draw a circle in the center of the frame
-        cv2.circle(frame, center=(centerX, centerY), radius=5, color=(0, 0, 255), thickness=-1)
+            # draw a circle in the center of the frame
+            cv2.circle(frame, center=(centerX, centerY), radius=5, color=(0, 0, 255), thickness=-1)
 
-        # find the object's location
-        frame_center = (centerX, centerY)
-        objectLoc = face_center.update(frame, frameCenter=None)
-        # print(centerX, centerY, objectLoc)
+            # find the object's location
+            frame_center = (centerX, centerY)
+            objectLoc = face_center.update(frame, frameCenter=None)
+            # print(centerX, centerY, objectLoc)
 
-        ((objX, objY), rect, d) = objectLoc
-        if d > 25 or d == -1:
-            # then either we got a false face, or we have no faces.
-            # the d - distance - value is used to keep the jitter down of false positive faces detected where there
-            #                   were none.
-            # if it is a false positive, or we cannot determine a distance, just stay put
-            # print(int(pan_update), int(tilt_update))
-            if track_face and fly:
-                tello.send_rc_control(0, 0, 0, 0)
-            continue  # ignore the sample as it is too far from the previous sample
-
-        if rect is not None:
-            (x, y, w, h) = rect
-            cv2.rectangle(frame, (x, y), (x + w, y + h),
-                          (0, 255, 0), 2)
-
-            # draw a circle in the center of the face
-            cv2.circle(frame, center=(objX, objY), radius=5, color=(255, 0, 0), thickness=-1)
-
-            # Draw line from frameCenter to face center
-            cv2.arrowedLine(frame, frame_center, (objX, objY), color=(0, 255, 0), thickness=2)
-
-            if run_pid:
-                # calculate the pan and tilt errors and run through pid controllers
-                pan_error = centerX - objX
-                pan_update = pan_pid.update(pan_error, sleep=0)
-
-                tilt_error = centerY - objY
-                tilt_update = tilt_pid.update(tilt_error, sleep=0)
-
-                # print(pan_error, int(pan_update), tilt_error, int(tilt_update))
-                cv2.putText(frame, f"X Error: {pan_error} PID: {pan_update:.2f}", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                            (0, 255, 0), 2, cv2.LINE_AA)
-
-                cv2.putText(frame, f"Y Error: {tilt_error} PID: {tilt_update:.2f}", (20, 70), cv2.FONT_HERSHEY_SIMPLEX,
-                            1,
-                            (0, 0, 255), 2, cv2.LINE_AA)
-
-                if pan_update > max_speed_threshold:
-                    pan_update = max_speed_threshold
-                elif pan_update < -max_speed_threshold:
-                    pan_update = -max_speed_threshold
-
-                # NOTE: if face is to the right of the drone, the distance will be negative, but
-                # the drone has to have positive power so I am flipping the sign
-                pan_update = pan_update * -1
-
-                if tilt_update > max_speed_threshold:
-                    tilt_update = max_speed_threshold
-                elif tilt_update < -max_speed_threshold:
-                    tilt_update = -max_speed_threshold
-
-                print(int(pan_update), int(tilt_update))
+            ((objX, objY), rect, d) = objectLoc
+            if d > 25 or d == -1:
+                # then either we got a false face, or we have no faces.
+                # the d - distance - value is used to keep the jitter down of false positive faces detected where there
+                #                   were none.
+                # if it is a false positive, or we cannot determine a distance, just stay put
+                # print(int(pan_update), int(tilt_update))
                 if track_face and fly:
-                    # left/right: -100/100
-                    tello.send_rc_control(pan_update // 3, 0, tilt_update // 2, 0)
+                    tello.send_rc_control(0, 0, 0, 0)
+                continue  # ignore the sample as it is too far from the previous sample
 
-        # send frame to other processes
-        show_video_conn.send(frame)
-        video_writer_conn.send(frame)
+            if rect is not None:
+                (x, y, w, h) = rect
+                cv2.rectangle(frame, (x, y), (x + w, y + h),
+                              (0, 255, 0), 2)
+
+                # draw a circle in the center of the face
+                cv2.circle(frame, center=(objX, objY), radius=5, color=(255, 0, 0), thickness=-1)
+
+                # Draw line from frameCenter to face center
+                cv2.arrowedLine(frame, frame_center, (objX, objY), color=(0, 255, 0), thickness=2)
+
+                if run_pid:
+                    # calculate the pan and tilt errors and run through pid controllers
+                    pan_error = centerX - objX
+                    pan_update = pan_pid.update(pan_error, sleep=0)
+
+                    tilt_error = centerY - objY
+                    tilt_update = tilt_pid.update(tilt_error, sleep=0)
+
+                    # print(pan_error, int(pan_update), tilt_error, int(tilt_update))
+                    cv2.putText(frame, f"X Error: {pan_error} PID: {pan_update:.2f}", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (0, 255, 0), 2, cv2.LINE_AA)
+
+                    cv2.putText(frame, f"Y Error: {tilt_error} PID: {tilt_update:.2f}", (20, 70), cv2.FONT_HERSHEY_SIMPLEX,
+                                1,
+                                (0, 0, 255), 2, cv2.LINE_AA)
+
+                    if pan_update > max_speed_threshold:
+                        pan_update = max_speed_threshold
+                    elif pan_update < -max_speed_threshold:
+                        pan_update = -max_speed_threshold
+
+                    # NOTE: if face is to the right of the drone, the distance will be negative, but
+                    # the drone has to have positive power so I am flipping the sign
+                    pan_update = pan_update * -1
+
+                    if tilt_update > max_speed_threshold:
+                        tilt_update = max_speed_threshold
+                    elif tilt_update < -max_speed_threshold:
+                        tilt_update = -max_speed_threshold
+
+                    if track_face and fly:
+                        # left/right: -100/100
+                        lr = int(max(-100, min(100, round(pan_update / 3))))
+                        ud = int(max(-100, min(100, round(tilt_update / 2))))
+                        print(lr, ud)
+                        tello.send_rc_control(lr, 0, ud, 0)
+
+            # send frame to other processes
+            show_video_conn.send(frame)
+            video_writer_conn.send(frame)
+    except Exception:
+        try:
+            if tello:
+                tello.send_rc_control(0, 0, 0, 0)
+                tello.land()
+        finally:
+            raise
     # then we got the exit event so cleanup
     signal_handler(None, None)
 
